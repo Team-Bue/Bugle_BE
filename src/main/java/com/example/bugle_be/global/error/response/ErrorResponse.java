15af29a6ef0522d7af246c03aabc189bd7866078
민수아 <Minsua0503@gmail.com>
2025-05-18 00:45:48 +0900

    (#1) 🌈 :: ErrorResponse 추가

---
+++ src/main/java/com/example/bugle_be/global/error/response/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.bugle_be.global.error.response;
+
+import com.example.bugle_be.global.error.exception.ErrorProperty;
+import org.springframework.http.HttpStatus;
+
+public record ErrorResponse(
+    HttpStatus status,
+    String message
+) {
+
+    public static ErrorResponse of(ErrorProperty errorProperty) {
+        return new ErrorResponse(
+            errorProperty.getStatus(),
+            errorProperty.getMessage()
+        );
+    }
+
+}
