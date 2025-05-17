f47f373c34c0d0ba84aa728beaf759177f7764c5
민수아 <Minsua0503@gmail.com>
2025-05-18 00:45:35 +0900

    (#1) 🌈 :: GlobalExceptionHandler 추가

---
+++ src/main/java/com/example/bugle_be/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.example.bugle_be.global.error;
+
+import com.example.bugle_be.global.error.exception.BugleException;
+import com.example.bugle_be.global.error.exception.ErrorProperty;
+import com.example.bugle_be.global.error.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BugleException.class)
+    public ResponseEntity<ErrorResponse> handleBugleException(BugleException e) {
+        final ErrorProperty errorProperty = e.getErrorProperty();
+        return ResponseEntity
+            .status(errorProperty.getStatus())
+            .body(ErrorResponse.of(errorProperty));
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> handleBindException(BindException e) {
+        Map<String, String> errorList = new HashMap<>();
+        for (FieldError fieldError : e.getFieldErrors()) {
+            errorList.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+    }
+
+}
