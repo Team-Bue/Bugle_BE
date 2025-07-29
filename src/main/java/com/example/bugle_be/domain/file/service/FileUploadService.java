a657b40ed3aff03eb0405f8cac6f9315d553365e
민수아 <Minsua0503@gmail.com>
2025-07-29 20:09:53 +0900

    (#3) 🌈 :: 유효성 검사 메서드 추가

--- src/main/java/com/example/bugle_be/domain/file/service/FileUploadService.java
+++ src/main/java/com/example/bugle_be/domain/file/service/FileUploadService.java
@@ -1,7 +1,10 @@
 package com.example.bugle_be.domain.file.service;
 
+import com.example.bugle_be.domain.file.exception.FileNotFound;
+import com.example.bugle_be.domain.file.exception.UnsupportedFileExtension;
 import com.example.bugle_be.domain.file.presentation.dto.request.FileUploadRequest;
 import com.example.bugle_be.domain.file.presentation.dto.response.FileUploadUrlResponse;
+import com.example.bugle_be.domain.file.type.FileType;
 import com.example.bugle_be.infra.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,20 +17,34 @@ import java.util.List;
 public class FileUploadService {
 
     private final S3Service s3Service;
+    private static final Duration DEFAULT_DURATION = Duration.ofMinutes(2);
 
     public FileUploadUrlResponse execute(FileUploadRequest request) {
-        Duration duration = Duration.ofMinutes(2);
-
         List<FileUploadUrlResponse.FileUploadUrl> urls = request.files().stream()
             .map(file -> {
+                validateFileName(file.fileName());
+                validateExtension(file.fileName(), file.fileType());
+
                 String fileName = file.fileName();
-                String url = s3Service.createUploadPresignedUrl(fileName, duration, file.fileType().getPath()).toString();
+                String url = s3Service.createUploadPresignedUrl(fileName, DEFAULT_DURATION, file.fileType().getPath()).toString();
 
                 return new FileUploadUrlResponse.FileUploadUrl(fileName, url);
             })
             .toList();
 
         return new FileUploadUrlResponse(urls);
+    }
+
+    private void validateFileName(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            throw FileNotFound.EXCEPTION;
+        }
+    }
+
+    private void validateExtension(String fileName, FileType fileType) {
+        if (!fileType.isAllowedExtension(fileName)) {
+            throw UnsupportedFileExtension.EXCEPTION;
+        }
     }
 
 }
