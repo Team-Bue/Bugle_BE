72f20c72af2eb7cca75e11694927b51d7c888c7f
민수아 <Minsua0503@gmail.com>
2025-07-29 20:08:48 +0900

    (#3) 🌈 :: UnsupportedFileExtension 추가

---
+++ src/main/java/com/example/bugle_be/domain/file/exception/UnsupportedFileExtension.java
@@ -0,0 +1,14 @@
+package com.example.bugle_be.domain.file.exception;
+
+import com.example.bugle_be.domain.file.exception.error.FileErrorCode;
+import com.example.bugle_be.global.error.exception.BugleException;
+
+public class UnsupportedFileExtension extends BugleException {
+
+    public static final BugleException EXCEPTION = new UnsupportedFileExtension();
+
+    private UnsupportedFileExtension() {
+        super(FileErrorCode.UNSUPPORTED_FILE_EXTENSION);
+    }
+
+}
