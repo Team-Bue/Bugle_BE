3f7f0db1c801a71ae4801c6b3239add745f0388c
민수아 <Minsua0503@gmail.com>
2025-07-31 22:19:22 +0900

    (#3) 🧹 :: GET용 presignedUrl 메서드 삭제

--- src/main/java/com/example/bugle_be/infra/s3/S3Service.java
+++ src/main/java/com/example/bugle_be/infra/s3/S3Service.java
@@ -2,10 +2,8 @@ package com.example.bugle_be.infra.s3;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.net.URL;
@@ -18,25 +16,10 @@ public class S3Service {
     private final S3Presigner s3Presigner;
     private final S3Properties s3Properties;
 
-    public URL createDownloadPresignedUrl(String objectKey, Duration expiration) {
-        GetObjectRequest request = GetObjectRequest.builder()
-            .bucket(s3Properties.bucket())
-            .key(objectKey)
-            .build();
-
-        GetObjectPresignRequest presignedRequest = GetObjectPresignRequest.builder()
-            .getObjectRequest(request)
-            .signatureDuration(expiration)
-            .build();
-
-        return s3Presigner.presignGetObject(presignedRequest).url();
-    }
-
-    public URL createUploadPresignedUrl(String objectKey, Duration expiration, String contentType) {
+    public URL createUploadPresignedUrl(String objectKey, Duration expiration) {
         PutObjectRequest request = PutObjectRequest.builder()
             .bucket(s3Properties.bucket())
             .key(objectKey)
-            .contentType(contentType)
             .build();
 
         PutObjectPresignRequest presignedRequest = PutObjectPresignRequest.builder()
