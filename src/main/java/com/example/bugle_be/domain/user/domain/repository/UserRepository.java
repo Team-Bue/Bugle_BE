b8815acda5782d35c3e51182467beda15b5afbd6
민수아 <Minsua0503@gmail.com>
2025-08-01 22:40:34 +0900

    (#9) 🌈 :: findByAccountId 쿼리 메서드 추가

--- src/main/java/com/example/bugle_be/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/bugle_be/domain/user/domain/repository/UserRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByAccountId(String accountId);
+
     Boolean existsByEmail(String email);
+
     Boolean existsByAccountId(String accountId);
 }
