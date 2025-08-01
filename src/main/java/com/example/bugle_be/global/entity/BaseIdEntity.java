e8c4b73d2fcc074a98c85b39d0cbd597c11c9e55
민수아 <Minsua0503@gmail.com>
2025-08-01 10:41:56 +0900

    (#5) 🌈 :: BaseIdEntity

---
+++ src/main/java/com/example/bugle_be/global/entity/BaseIdEntity.java
@@ -0,0 +1,17 @@
+package com.example.bugle_be.global.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class BaseIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
