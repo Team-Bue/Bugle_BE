55684b38f855e5a65e4a31204f3e0c2e22dca366
민수아 <Minsua0503@gmail.com>
2025-08-04 22:57:47 +0900

    (#19) 🌈 :: QueryPostListService

--- src/main/java/com/example/bugle_be/domain/post/presentation/PostController.java
+++ src/main/java/com/example/bugle_be/domain/post/presentation/PostController.java
@@ -1,7 +1,9 @@
 package com.example.bugle_be.domain.post.presentation;
 
 import com.example.bugle_be.domain.post.presentation.dto.request.PostRequest;
+import com.example.bugle_be.domain.post.presentation.dto.response.QueryPostListResponse;
 import com.example.bugle_be.domain.post.service.CreatePostService;
+import com.example.bugle_be.domain.post.service.QueryPostListService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,10 +15,17 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final CreatePostService createPostService;
+    private final QueryPostListService queryPostListService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody @Valid PostRequest request) {
         createPostService.execute(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public QueryPostListResponse queryPostList() {
+        return queryPostListService.execute();
     }
 }
