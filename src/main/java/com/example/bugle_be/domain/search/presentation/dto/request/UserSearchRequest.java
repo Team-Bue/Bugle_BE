f81d92bc791726b51acb0a7847bbe5dc043dbc93
민수아 <Minsua0503@gmail.com>
2025-08-06 10:03:08 +0900

    (#21) 🌈 :: UserSearchRequest

---
+++ src/main/java/com/example/bugle_be/domain/search/presentation/dto/request/UserSearchRequest.java
@@ -0,0 +1,10 @@
+package com.example.bugle_be.domain.search.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UserSearchRequest(
+
+    @NotBlank(message = "검색 키워드는 필수입니다.")
+    String keyword
+) {
+}
