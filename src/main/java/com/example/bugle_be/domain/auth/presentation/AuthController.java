eb9e92aec802dca4593ebbbe3238891d9d669432
민수아 <Minsua0503@gmail.com>
2025-08-09 22:56:43 +0900

    (#26) 🌈 :: LogoutService

--- src/main/java/com/example/bugle_be/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/bugle_be/domain/auth/presentation/AuthController.java
@@ -4,6 +4,7 @@ import com.example.bugle_be.domain.auth.presentation.dto.request.LoginRequest;
 import com.example.bugle_be.domain.auth.presentation.dto.request.SignupRequest;
 import com.example.bugle_be.domain.auth.presentation.dto.response.TokenResponse;
 import com.example.bugle_be.domain.auth.service.LoginService;
+import com.example.bugle_be.domain.auth.service.LogoutService;
 import com.example.bugle_be.domain.auth.service.SignupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ public class AuthController {
 
     private final SignupService signupService;
     private final LoginService loginService;
+    private final LogoutService logoutService;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,5 +30,11 @@ public class AuthController {
     @ResponseStatus(HttpStatus.OK)
     public TokenResponse login(@RequestBody @Valid LoginRequest request) {
         return loginService.execute(request);
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout() {
+        logoutService.execute();
     }
 }
---
+++ src/main/java/com/example/bugle_be/domain/auth/service/LogoutService.java
@@ -0,0 +1,27 @@
+package com.example.bugle_be.domain.auth.service;
+
+import com.example.bugle_be.domain.auth.domain.RefreshToken;
+import com.example.bugle_be.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.bugle_be.domain.auth.exception.RefreshTokenNotFound;
+import com.example.bugle_be.domain.user.facacde.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService {
+
+    private final UserFacade userFacade;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public void execute() {
+        String email = userFacade.getCurrentUser().getEmail();
+
+        RefreshToken refreshToken = refreshTokenRepository.findById(email)
+            .orElseThrow(() -> RefreshTokenNotFound.EXCEPTION);
+
+        refreshTokenRepository.delete(refreshToken);
+    }
+}
