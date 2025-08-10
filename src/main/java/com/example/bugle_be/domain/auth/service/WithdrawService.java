12d715c3d70901a8e9db4be0f2003d4d92ae5dd1
민수아 <Minsua0503@gmail.com>
2025-08-10 15:54:42 +0900

    (#26) 🌈 :: 회원탈퇴 시 refreshToken 삭제 및 securityContext 정리

--- src/main/java/com/example/bugle_be/domain/auth/service/WithdrawService.java
+++ src/main/java/com/example/bugle_be/domain/auth/service/WithdrawService.java
@@ -1,9 +1,11 @@
 package com.example.bugle_be.domain.auth.service;
 
+import com.example.bugle_be.domain.auth.domain.repository.RefreshTokenRepository;
 import com.example.bugle_be.domain.user.domain.User;
 import com.example.bugle_be.domain.user.domain.repository.UserRepository;
 import com.example.bugle_be.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +15,15 @@ public class WithdrawService {
 
     private final UserFacade userFacade;
     private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public void execute() {
         User user = userFacade.getCurrentUser();
 
+        refreshTokenRepository.deleteById(user.getEmail());
         userRepository.delete(user);
+
+        SecurityContextHolder.clearContext();
     }
 }
