52f395b351e5503ceefb27dbbc8c6e39105c36f0
민수아 <Minsua0503@gmail.com>
2025-08-26 11:35:29 +0900

    (#36) 🧹 :: 불필요한 쿼리 메서드 제거

--- src/main/java/com/example/bugle_be/domain/mail/domain/repository/VerificationCodeRepository.java
+++ src/main/java/com/example/bugle_be/domain/mail/domain/repository/VerificationCodeRepository.java
@@ -4,6 +4,4 @@ import com.example.bugle_be.domain.mail.domain.VerificationCode;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VerificationCodeRepository extends CrudRepository<VerificationCode, String> {
-
-    int deleteByEmailAndCode(String email, String code);
 }
