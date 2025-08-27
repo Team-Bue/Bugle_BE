d2ea434cc80974f683fd371f0260c4a0c2560018
민수아 <Minsua0503@gmail.com>
2025-08-27 17:57:18 +0900

    (#37) 🌈 :: 회원가입 시 메일 인증 로직 추가

--- src/main/java/com/example/bugle_be/domain/auth/service/SignupService.java
+++ src/main/java/com/example/bugle_be/domain/auth/service/SignupService.java
@@ -4,6 +4,7 @@ import com.example.bugle_be.domain.auth.exception.AlreadyAccountIdExists;
 import com.example.bugle_be.domain.auth.exception.AlreadyEmailExists;
 import com.example.bugle_be.domain.auth.presentation.dto.request.SignupRequest;
 import com.example.bugle_be.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.bugle_be.domain.mail.service.MailService;
 import com.example.bugle_be.domain.user.domain.User;
 import com.example.bugle_be.domain.user.domain.repository.UserRepository;
 import com.example.bugle_be.domain.user.profile.DefaultImageProperties;
@@ -21,9 +22,12 @@ public class SignupService {
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
     private final DefaultImageProperties defaultImage;
+    private final MailService mailService;
 
     @Transactional
     public TokenResponse execute(SignupRequest request) {
+        mailService.validateToken(request.email(), request.token());
+
         checkDuplicate(request);
         saveUser(request);
 
