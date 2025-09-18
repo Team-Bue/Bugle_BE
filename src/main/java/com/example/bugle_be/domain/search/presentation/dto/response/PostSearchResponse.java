e457d1619d915edad03374c1958cb0c40fe04cf3
민수아 <Minsua0503@gmail.com>
2025-09-18 22:53:36 +0900

    (#40) 🧹 :: class -> record로 변경

--- src/main/java/com/example/bugle_be/domain/search/presentation/dto/response/PostSearchResponse.java
+++ src/main/java/com/example/bugle_be/domain/search/presentation/dto/response/PostSearchResponse.java
@@ -1,18 +1,16 @@
 package com.example.bugle_be.domain.search.presentation.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.Getter;
 
 import java.util.List;
 
 public record PostSearchResponse(
     List<PostResponse> posts
 ) {
-    @Getter
-    public static class PostResponse {
-        private final Long id;
-        private final String fileUrl;
-
+    public record PostResponse(
+        Long id,
+        String fileUrl
+    ) {
         @QueryProjection
         public PostResponse(Long id, String fileUrl) {
             this.id = id;
