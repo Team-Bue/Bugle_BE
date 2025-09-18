01e6efda39110649b32dcba13a02a9f774590538
민수아 <Minsua0503@gmail.com>
2025-09-18 23:54:12 +0900

    (#40) 🌈 :: 게시글 검색 페이지 개수 조회 추가

--- src/main/java/com/example/bugle_be/domain/search/presentation/SearchController.java
+++ src/main/java/com/example/bugle_be/domain/search/presentation/SearchController.java
@@ -6,7 +6,9 @@ import com.example.bugle_be.domain.search.presentation.dto.response.PostSearchRe
 import com.example.bugle_be.domain.search.presentation.dto.response.UserSearchResponse;
 import com.example.bugle_be.domain.search.service.PostSearchService;
 import com.example.bugle_be.domain.search.service.UserSearchService;
+import com.example.bugle_be.global.dto.TotalPageCountResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,8 +29,16 @@ public class SearchController {
 
     @GetMapping("/posts")
     public PostSearchResponse postSearch(
+        @RequestParam(value = "page", required = false, defaultValue = "1") @Positive Integer page,
         @Valid @ModelAttribute PostSearchRequest request
     ) {
-        return postSearchService.execute(request);
+        return postSearchService.execute(page, request);
+    }
+
+    @GetMapping("/posts/count")
+    public TotalPageCountResponse postSearchCount(
+        @Valid @ModelAttribute PostSearchRequest request
+    ) {
+        return postSearchService.executeCount(request);
     }
 }
