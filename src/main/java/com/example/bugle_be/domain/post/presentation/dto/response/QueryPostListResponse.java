b1fe9f5d196368e6362b932e2cddbb8e872da869
민수아 <Minsua0503@gmail.com>
2025-09-18 22:52:42 +0900

    (#40) 🌈 :: QueryProjection 적용

--- src/main/java/com/example/bugle_be/domain/post/presentation/dto/response/QueryPostListResponse.java
+++ src/main/java/com/example/bugle_be/domain/post/presentation/dto/response/QueryPostListResponse.java
@@ -1,5 +1,6 @@
 package com.example.bugle_be.domain.post.presentation.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 
 import java.util.List;
@@ -16,5 +17,16 @@ public record QueryPostListResponse(
         String region,
         String fileUrl,
         String content
-    ) {}
+    ) {
+        @QueryProjection
+        public PostPreviewResponse(Long id, String accountId, String profileImageUrl, String country, String region, String fileUrl, String content) {
+            this.id = id;
+            this.accountId = accountId;
+            this.profileImageUrl = profileImageUrl;
+            this.country = country;
+            this.region = region;
+            this.fileUrl = fileUrl;
+            this.content = content;
+        }
+    }
 }
