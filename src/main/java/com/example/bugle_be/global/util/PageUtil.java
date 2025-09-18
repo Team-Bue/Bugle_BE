f8ad425eed9c411e8f05632feed20519b73515af
민수아 <Minsua0503@gmail.com>
2025-09-18 23:23:18 +0900

    (#40) 🌈 :: PageUtil

---
+++ src/main/java/com/example/bugle_be/global/util/PageUtil.java
@@ -0,0 +1,17 @@
+package com.example.bugle_be.global.util;
+
+import com.example.bugle_be.global.exception.InvalidPaginationParameter;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageUtil {
+    public static final int POST_DEFAULT_PAGE_SIZE = 20;
+
+    public static int getTotalPageCount(Long totalCount, int limit) {
+        if (totalCount == null || limit <= 0) {
+            throw InvalidPaginationParameter.EXCEPTION;
+        }
+        return (int) Math.ceil(totalCount.doubleValue() / limit);
+    }
+}
