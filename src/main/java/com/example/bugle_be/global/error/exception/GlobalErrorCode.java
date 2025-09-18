243ee7d960e87185bd3234eaf872db2949faf631
민수아 <Minsua0503@gmail.com>
2025-09-18 23:22:54 +0900

    (#40) 🌈 :: InvalidPaginationParameter Exception

--- src/main/java/com/example/bugle_be/global/error/exception/GlobalErrorCode.java
+++ src/main/java/com/example/bugle_be/global/error/exception/GlobalErrorCode.java
@@ -11,7 +11,9 @@ public enum GlobalErrorCode implements ErrorProperty {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
 
     EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired JWT"),
-    INVALID_JWT(HttpStatus.UNAUTHORIZED, "Invalid JWT");
+    INVALID_JWT(HttpStatus.UNAUTHORIZED, "Invalid JWT"),
+
+    INVALID_PAGINATION_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid pagination parameter");
 
     private final HttpStatus status;
     private final String message;
