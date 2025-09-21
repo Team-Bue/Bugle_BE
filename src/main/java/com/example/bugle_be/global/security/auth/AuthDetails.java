aef5b48af96d139d72ede459b5427f2c8f028472
민수아 <Minsua0503@gmail.com>
2025-09-21 20:02:54 +0900

    (#28) 🌈 :: Oauth2User도 구현하도록 추가

--- src/main/java/com/example/bugle_be/global/security/auth/AuthDetails.java
+++ src/main/java/com/example/bugle_be/global/security/auth/AuthDetails.java
@@ -4,14 +4,17 @@ import com.example.bugle_be.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 @RequiredArgsConstructor
-public class AuthDetails implements UserDetails {
+public class AuthDetails implements UserDetails, OAuth2User {
 
     private final User user;
+    private final Map<String, Object> attributes;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -46,5 +49,20 @@ public class AuthDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
     }
 }
