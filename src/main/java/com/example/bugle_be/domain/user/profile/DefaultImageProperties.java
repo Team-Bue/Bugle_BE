9cecab36ec4af71a9e849277ee470ee47b6e5745
민수아 <Minsua0503@gmail.com>
2025-09-23 18:10:13 +0900

    (#44) 🧹 :: DefaultImageProperties 삭제

--- src/main/java/com/example/bugle_be/domain/user/profile/DefaultImageProperties.java
+++
@@ -1,9 +0,0 @@
-package com.example.bugle_be.domain.user.profile;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "user.profile")
-public record DefaultImageProperties(
-    String defaultImageUrl
-) {
-}
