ae1957e07bb191ea93a67bb455e57e0a49ac8385
민수아 <Minsua0503@gmail.com>
2025-10-13 10:28:42 +0900

    (#48) 🌈 :: PostRequest

---
+++ src/main/java/com/example/bugle_be/domain/comment/presentation/dto/request/PostRequest.java
@@ -0,0 +1,12 @@
+package com.example.bugle_be.domain.comment.presentation.dto.request;
+
+import com.example.bugle_be.global.util.MessageProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostRequest(
+    @NotBlank(message = MessageProperty.COMMENT_NOT_BLANK)
+    @Size(max = 300, message = MessageProperty.COMMENT_SIZE)
+    String content
+) {
+}
