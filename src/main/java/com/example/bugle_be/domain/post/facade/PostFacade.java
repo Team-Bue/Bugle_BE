96a4008dabfff0d19fead11ce38bb96b57f7e05b
민수아 <Minsua0503@gmail.com>
2025-10-15 19:21:18 +0900

    (#54) 🌈 :: PostFacade

---
+++ src/main/java/com/example/bugle_be/domain/post/facade/PostFacade.java
@@ -0,0 +1,19 @@
+package com.example.bugle_be.domain.post.facade;
+
+import com.example.bugle_be.domain.post.domain.Post;
+import com.example.bugle_be.domain.post.domain.repository.PostRepository;
+import com.example.bugle_be.domain.post.exception.PostNotFound;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostFacade {
+
+    private final PostRepository postRepository;
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id)
+            .orElseThrow(() -> PostNotFound.EXCEPTION);
+    }
+}
