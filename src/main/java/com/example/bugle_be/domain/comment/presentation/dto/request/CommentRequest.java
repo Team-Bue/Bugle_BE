7a64a869417cb7247ecfa95ba72617d486b45cf6
민수아 <Minsua0503@gmail.com>
2025-10-15 13:36:48 +0900

    (#48) 🧹 :: Post -> Comment로 변경

--- src/main/java/com/example/bugle_be/domain/comment/presentation/dto/request/CommentRequest.java
+++ src/main/java/com/example/bugle_be/domain/comment/presentation/dto/request/CommentRequest.java
@@ -4,7 +4,7 @@ import com.example.bugle_be.global.util.MessageProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record PostRequest(
+public record CommentRequest(
     @NotBlank(message = MessageProperty.COMMENT_NOT_BLANK)
     @Size(max = 300, message = MessageProperty.COMMENT_SIZE)
     String content
