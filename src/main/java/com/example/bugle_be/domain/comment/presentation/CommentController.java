21281b92e79cec0ad44df3d4ac2d050a6d36725b
민수아 <Minsua0503@gmail.com>
2025-10-15 14:15:25 +0900

    (#48) 🧹 :: @Valid 추가 및 와일드 카드 제거

--- src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
@@ -2,9 +2,15 @@ package com.example.bugle_be.domain.comment.presentation;
 
 import com.example.bugle_be.domain.comment.presentation.dto.request.CommentRequest;
 import com.example.bugle_be.domain.comment.service.CreateCommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/comments")
@@ -15,7 +21,7 @@ public class CommentController {
 
     @PostMapping("/{post-id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@PathVariable("post-id") Long postId, @RequestBody CommentRequest request) {
+    public void create(@PathVariable("post-id") Long postId, @RequestBody @Valid CommentRequest request) {
         createCommentService.execute(postId, request);
     }
 }
