817601f09d11732333a0739e919344b8b661c0fa
민수아 <Minsua0503@gmail.com>
2025-10-16 15:05:13 +0900

    (#58) 🧹 :: 네이밍 변경

--- src/main/java/com/example/bugle_be/domain/post/domain/repository/PostRepositoryCustom.java
+++ src/main/java/com/example/bugle_be/domain/post/domain/repository/PostRepositoryCustom.java
@@ -1,6 +1,6 @@
 package com.example.bugle_be.domain.post.domain.repository;
 
-import com.example.bugle_be.domain.post.presentation.dto.response.QueryPostListResponse;
+import com.example.bugle_be.domain.post.presentation.dto.response.PostsResponse;
 import com.example.bugle_be.domain.search.presentation.dto.SearchType;
 import com.example.bugle_be.domain.search.presentation.dto.response.PostSearchResponse;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface PostRepositoryCustom {
 
-    List<QueryPostListResponse.PostPreviewResponse> getAll(int page);
+    List<PostsResponse.PostPreviewResponse> getAll(int page);
 
     Long getAllCount();
 
