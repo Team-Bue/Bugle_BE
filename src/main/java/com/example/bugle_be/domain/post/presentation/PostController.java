9f03e51c25c409539f6e72ede55c6d65444767e1
민수아 <Minsua0503@gmail.com>
2025-10-16 10:41:53 +0900

    (#56) 🌈 :: DeletePostService

--- src/main/java/com/example/bugle_be/domain/post/presentation/PostController.java
+++ src/main/java/com/example/bugle_be/domain/post/presentation/PostController.java
@@ -12,15 +12,7 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
@@ -30,6 +22,7 @@ public class PostController {
 
     private final CreatePostService createPostService;
     private final UpdatePostService updatePostService;
+    private final DeletePostService deletePostService;
     private final QueryPostListService queryPostListService;
 
     @PostMapping
@@ -42,6 +35,12 @@ public class PostController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@PathVariable("post-id") Long postId, @RequestBody @Valid PostRequest request) {
         updatePostService.execute(postId, request);
+    }
+
+    @DeleteMapping("/{post-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("post-id") Long postId) {
+        deletePostService.execute(postId);
     }
 
     @GetMapping
