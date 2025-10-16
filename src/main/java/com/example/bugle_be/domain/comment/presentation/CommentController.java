706ca54da1e522c898165828fefb86cbe1dba3b4
민수아 <Minsua0503@gmail.com>
2025-10-16 15:05:28 +0900

    (#58) 🧹 :: 와일드 카드 제거

--- src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
@@ -7,7 +7,14 @@ import com.example.bugle_be.domain.comment.service.UpdateCommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/comments")
