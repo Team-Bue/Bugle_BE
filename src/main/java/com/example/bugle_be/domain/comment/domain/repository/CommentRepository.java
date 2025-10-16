2eefeecd3cf1d85e8ef11c01e2896a91df3fee2a
민수아 <Minsua0503@gmail.com>
2025-10-16 15:48:11 +0900

    (#58) 🌈 :: CommentRepositoryCustom

--- src/main/java/com/example/bugle_be/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/bugle_be/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,5 @@ package com.example.bugle_be.domain.comment.domain.repository;
 import com.example.bugle_be.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 }
---
+++ src/main/java/com/example/bugle_be/domain/comment/domain/repository/CommentRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.example.bugle_be.domain.comment.domain.repository;
+
+import com.example.bugle_be.domain.comment.presentation.dto.response.CommentsResponse;
+
+import java.util.List;
+
+public interface CommentRepositoryCustom {
+
+    List<CommentsResponse.CommentResponse> getAll(Long postId);
+}
