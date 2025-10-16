098a35a5dbb2b79a1c93a1bc97689fd08e20557e
민수아 <Minsua0503@gmail.com>
2025-10-16 15:48:35 +0900

    (#58) 🌈 :: QueryCommentsService

--- src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/bugle_be/domain/comment/presentation/CommentController.java
@@ -1,8 +1,10 @@
 package com.example.bugle_be.domain.comment.presentation;
 
 import com.example.bugle_be.domain.comment.presentation.dto.request.CommentRequest;
+import com.example.bugle_be.domain.comment.presentation.dto.response.CommentsResponse;
 import com.example.bugle_be.domain.comment.service.CreateCommentService;
 import com.example.bugle_be.domain.comment.service.DeleteCommentService;
+import com.example.bugle_be.domain.comment.service.QueryCommentsService;
 import com.example.bugle_be.domain.comment.service.UpdateCommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/comments")
@@ -24,6 +27,7 @@ public class CommentController {
     private final CreateCommentService createCommentService;
     private final UpdateCommentService updateCommentService;
     private final DeleteCommentService deleteCommentService;
+    private final QueryCommentsService queryCommentsService;
 
     @PostMapping("/{post-id}")
     @ResponseStatus(HttpStatus.CREATED)
@@ -41,5 +45,11 @@ public class CommentController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("comment-id") Long commentId) {
         deleteCommentService.execute(commentId);
+    }
+
+    @GetMapping("/{post-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CommentsResponse queryAll(@PathVariable("post-id") Long postId) {
+        return queryCommentsService.execute(postId);
     }
 }
