a1d50a0a8333ea0c3aaf751535db4efe647e2f0e
민수아 <Minsua0503@gmail.com>
2025-10-16 20:51:44 +0900

    (#60) 🌈 :: facade 적용

--- src/main/java/com/example/bugle_be/domain/comment/service/DeleteCommentService.java
+++ src/main/java/com/example/bugle_be/domain/comment/service/DeleteCommentService.java
@@ -3,7 +3,7 @@ package com.example.bugle_be.domain.comment.service;
 import com.example.bugle_be.domain.comment.domain.Comment;
 import com.example.bugle_be.domain.comment.domain.repository.CommentRepository;
 import com.example.bugle_be.domain.comment.exception.CannotDeleteComment;
-import com.example.bugle_be.domain.comment.exception.CommentNotFound;
+import com.example.bugle_be.domain.comment.facade.CommentFacade;
 import com.example.bugle_be.domain.user.domain.User;
 import com.example.bugle_be.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -14,13 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeleteCommentService {
 
-    private final CommentRepository commentRepository;
+    private final CommentFacade commentFacade;
     private final UserFacade userFacade;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public void execute(Long commentId) {
-        Comment comment = commentRepository.findById(commentId)
-            .orElseThrow(() -> CommentNotFound.EXCEPTION);
+        Comment comment = commentFacade.getCommentById(commentId);
         User user = userFacade.getCurrentUser();
 
         if (!comment.getUser().getId().equals(user.getId())) {
