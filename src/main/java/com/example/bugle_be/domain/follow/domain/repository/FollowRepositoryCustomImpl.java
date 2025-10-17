4599aa4a7de25a8cdf1e77ec64e8ef44ee422d53
민수아 <Minsua0503@gmail.com>
2025-10-17 19:12:36 +0900

    (#62) 🌈 :: FollowRepositoryCustomImpl

---
+++ src/main/java/com/example/bugle_be/domain/follow/domain/repository/FollowRepositoryCustomImpl.java
@@ -0,0 +1,36 @@
+package com.example.bugle_be.domain.follow.domain.repository;
+
+import static com.example.bugle_be.domain.user.domain.QUser.user;
+import static com.example.bugle_be.domain.follow.domain.QFollow.follow;
+
+import com.example.bugle_be.domain.follow.presentation.dto.response.FollowResponse;
+import com.example.bugle_be.domain.follow.presentation.dto.response.QFollowResponse_UserDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class FollowRepositoryCustomImpl implements FollowRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<FollowResponse.UserDto> findAllFollowersByUserId(Long userId) {
+        return queryFactory
+            .select(
+                new QFollowResponse_UserDto(
+                    user.id,
+                    user.accountId,
+                    user.userName,
+                    user.profileImageUrl
+                )
+            )
+            .from(follow)
+            .join(follow.following, user)
+            .where(follow.follower.id.eq(userId))
+            .fetch();
+    }
+}
