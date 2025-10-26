a0829569d4ff22a9f2b07a439b761bdde911a794
민수아 <Minsua0503@gmail.com>
2025-10-26 20:38:39 +0900

    (#64) 🌈 :: FCMErrorCode

---
+++ src/main/java/com/example/bugle_be/infra/fcm/exception/error/FCMErrorCode.java
@@ -0,0 +1,16 @@
+package com.example.bugle_be.infra.fcm.exception.error;
+
+import com.example.bugle_be.global.error.exception.ErrorProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum FCMErrorCode implements ErrorProperty {
+
+    DEVICE_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Device token not found");
+
+    private final HttpStatus status;
+    private final String message;
+}
