b751029e9fa1ad4804d594754017e0a1c7fec876
민수아 <Minsua0503@gmail.com>
2025-10-26 20:38:46 +0900

    (#64) 🌈 :: FCMService

---
+++ src/main/java/com/example/bugle_be/infra/fcm/service/FCMService.java
@@ -0,0 +1,37 @@
+package com.example.bugle_be.infra.fcm.service;
+
+import com.example.bugle_be.domain.notification.domain.Notification;
+import com.example.bugle_be.infra.fcm.exception.DeviceTokenNotFound;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FCMService {
+
+    public void sendMessage(String token, Notification notification) {
+        if (token == null || token.isEmpty()) {
+            throw DeviceTokenNotFound.EXCEPTION;
+        }
+
+        try {
+            Message message = Message.builder()
+                .setToken(token)
+                .setNotification(com.google.firebase.messaging.Notification.builder()
+                    .setTitle(notification.getTitle())
+                    .setBody(notification.getContent())
+                    .build())
+                .build();
+
+            FirebaseMessaging.getInstance().send(message);
+        } catch (FirebaseMessagingException e) {
+            log.error("Failed to send message", e);
+        }
+    }
+}
