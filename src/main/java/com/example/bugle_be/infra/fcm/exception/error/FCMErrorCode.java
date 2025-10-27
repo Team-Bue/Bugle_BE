9678711140747044cf419ac2e833afb7f3254fc0
민수아 <Minsua0503@gmail.com>
2025-10-27 16:48:55 +0900

    (#64) 🌈 :: FCMInitializationFailed

--- src/main/java/com/example/bugle_be/infra/fcm/exception/error/FCMErrorCode.java
+++ src/main/java/com/example/bugle_be/infra/fcm/exception/error/FCMErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FCMErrorCode implements ErrorProperty {
 
-    DEVICE_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Device token not found");
+    DEVICE_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Device token not found"),
+    FCM_INITIALIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FCM initialization failed");
 
     private final HttpStatus status;
     private final String message;
