11f8e6491c6ed224e5027f644b472e6664276a90
민수아 <Minsua0503@gmail.com>
2025-10-27 16:48:42 +0900

    (#64) 🧹 :: 로그 대신 예외처리로 변경

--- src/main/java/com/example/bugle_be/infra/fcm/config/FirebaseConfig.java
+++ src/main/java/com/example/bugle_be/infra/fcm/config/FirebaseConfig.java
@@ -1,17 +1,16 @@
 package com.example.bugle_be.infra.fcm.config;
 
+import com.example.bugle_be.infra.fcm.exception.FCMInitializationFailed;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
-@Slf4j
 @Configuration
 public class FirebaseConfig {
 
@@ -35,7 +34,7 @@ public class FirebaseConfig {
                 FirebaseApp.initializeApp(options);
             }
         } catch (Exception e) {
-            log.error("FCM initialization failed", e);
+            throw FCMInitializationFailed.EXCEPTION;
         }
     }
 }
