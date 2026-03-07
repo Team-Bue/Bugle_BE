2721833cceacab4e9432091cd078d53cb649727a
민수아 <Minsua0503@gmail.com>
2026-03-07 20:37:22 +0900

    (#105) 🧹 :: 중복되는 서비스 하나로 통합

--- src/main/java/com/example/bugle_be/infra/elasticsearch/event/PostIndexEventListener.java
+++ src/main/java/com/example/bugle_be/infra/elasticsearch/event/PostIndexEventListener.java
@@ -1,8 +1,7 @@
 package com.example.bugle_be.infra.elasticsearch.event;
 
-import com.example.bugle_be.infra.elasticsearch.post.service.CreatePostIndexService;
 import com.example.bugle_be.infra.elasticsearch.post.service.DeletePostIndexService;
-import com.example.bugle_be.infra.elasticsearch.post.service.UpdatePostIndexService;
+import com.example.bugle_be.infra.elasticsearch.post.service.SavePostIndexService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -15,8 +14,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class PostIndexEventListener {
 
-    private final CreatePostIndexService createPostIndexService;
-    private final UpdatePostIndexService updatePostIndexService;
+    private final SavePostIndexService savePostIndexService;
     private final DeletePostIndexService deletePostIndexService;
 
     @Async("elasticsearchAsyncExecutor")
@@ -24,8 +22,7 @@ public class PostIndexEventListener {
     public void handle(PostIndexEvent event) {
         try {
             switch (event.action()) {
-                case CREATE -> createPostIndexService.execute(event);
-                case UPDATE -> updatePostIndexService.execute(event);
+                case CREATE, UPDATE -> savePostIndexService.execute(event);
                 case DELETE -> deletePostIndexService.execute(event);
             }
         } catch (Exception e) {
--- src/main/java/com/example/bugle_be/infra/elasticsearch/post/service/SavePostIndexService.java
+++ src/main/java/com/example/bugle_be/infra/elasticsearch/post/service/SavePostIndexService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UpdatePostIndexService {
+public class SavePostIndexService {
 
     private final PostDocumentRepository postDocumentRepository;
 
